b26917e78ca1ab7f7a57879043d27a6c404fd5c8
GuiAz <guicottas@hotmail.com>
2026-04-05 07:02:32 -0300

    Analise da Strings em javaScript

---
+++ exercicios-js/fundamentos/templateString.js
@@ -0,0 +1,15 @@
+const nome = "Rebeca";
+const concatenacao = "Olá " + nome + "!";
+
+const template = `
+Olá
+${nome}!
+`;
+console.log(concatenacao, template);
+
+// expressoes...
+console.log(`1 + 1 = ${1 + 1}`);
+s;
+
+const up = (texto) => texto.toUpperCase();
+console.log(`Ei... ${up("cuidado")}!`);
